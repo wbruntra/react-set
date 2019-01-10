f0d1c80bba0a656d2969b2da8dfded3146d5e204
William Bruntrager <w.bruntrager@gmail.com>
2019-01-10 09:47:57 -0500

    Spruce up main menu

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Card from './Card';
 
 class Main extends Component {
-
   render() {
     return (
       <div className="container">
-        <ul className="collection">
+        <h2>Main Menu</h2>
+        <div className="row">
+          <div className="col s3">
+            <Link to="/solo">
+              <div className="card">
+                <Card desc="0012" />
+              </div>
+            </Link>
+            <p className="center-align">Play Solo</p>
+          </div>
+          <div className="col s3">
+            <Link to="/lobby">
+              <div className="card">
+                <Card desc="1121" />
+              </div>
+            </Link>
+            <p className="center-align">Join Game</p>
+          </div>
+          <div className="col s3">
+            <Link to="/host">
+              <div className="card">
+                <Card desc="2200" />
+              </div>
+            </Link>
+            <p className="center-align">Host Game</p>
+          </div>
+        </div>
+        {/* <ul className="collection">
           <li className="collection-item">
             <Link to="/host">Host</Link>
           </li>
@@ -16,7 +43,7 @@ class Main extends Component {
           <li className="collection-item">
             <Link to="/solo">Solo</Link>
           </li>
-        </ul>
+        </ul> */}
       </div>
     );
   }
