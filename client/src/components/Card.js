ef62c33a5d46acbd613b0b25ad2600e41d4b642b
William Bruntrager <w.bruntrager@gmail.com>
2019-09-23 07:37:35 -0400

    Replace rectangle with squiggle

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -9,29 +9,63 @@ const config = {
   padding: 25,
 }
 
+const Rectangle = ({ fill, color }) => {
+  let { padding, height, width, strokeWidth } = config
+  return (
+    <g>
+      {/* triangle */}
+      {/* <polygon
+    points={`${padding},${height - padding}
+    ${width / 2},${padding}
+    ${width - padding},${height - padding}`}
+    style={{ fill: fill, stroke: color, strokeWidth: config.strokeWidth }}
+  /> */}
+      {/* rectangle */}
+      <polygon
+        points={`${padding},${padding}
+    ${width - padding},${padding}
+    ${width - padding},${height - padding}
+    ${padding},${height - padding}`}
+        style={{ fill: fill, stroke: color, strokeWidth: strokeWidth }}
+      />
+    </g>
+  )
+}
+
+const Squiggle = ({ fill, color }) => {
+  let { padding, height, width, strokeWidth } = config
+  return (
+    <svg width="120" height="200" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <rect fill="#fff" id="canvas_background" height="202" width="122" y="-1" x="-1" />
+        <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+          <rect 
+          fill="#fff"
+          strokeWidth="0" 
+          y="0" x="0" height="100%" width="100%" />
+        </g>
+      </g>
+      <g>
+        <path
+          stroke={color}
+          transform="rotate(89.69688415527344 58.75771713256837,97.92066192626952) "
+          id="svg_5"
+          d="m-17.485088,66.013951c50.828541,-35.451902 101.657071,35.451899 152.485601,0l0,63.813412c-50.82853,35.451904 -101.657061,-35.451896 -152.485601,0l0,-63.813412z"
+          fillOpacity="null"
+          strokeOpacity="null"
+          strokeWidth={strokeWidth}
+          fill={fill}
+        />
+      </g>
+    </svg>
+  )
+}
+
 const Shape = ({ shape, fill, color }) => {
   let { padding, height, width, strokeWidth } = config
   if (shape === '0') {
     padding = padding + 1
-    return (
-      <g>
-        {/* triangle */}
-        {/* <polygon
-          points={`${padding},${height - padding}
-          ${width / 2},${padding}
-          ${width - padding},${height - padding}`}
-          style={{ fill: fill, stroke: color, strokeWidth: config.strokeWidth }}
-        /> */}
-        {/* rectangle */}
-        <polygon
-          points={`${padding},${padding}
-          ${width - padding},${padding}
-          ${width - padding},${height - padding}
-          ${padding},${height - padding}`}
-          style={{ fill: fill, stroke: color, strokeWidth: strokeWidth }}
-        />
-      </g>
-    )
+    return <Squiggle fill={fill} color={color} />
   }
   if (shape === '1') {
     return (
@@ -53,6 +87,8 @@ const Shape = ({ shape, fill, color }) => {
   if (shape === '2') {
     return (
       <g>
+        {/* diamond */}
+
         <polygon
           points={`${padding},${height / 2}
           ${width / 2},${padding}
