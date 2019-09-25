3d7b05e6d685b9828a99c4c944cae61ead5142f4
William Bruntrager <w.bruntrager@gmail.com>
2019-09-25 03:11:36 -0400

    Add build folder

---
+++ client/build/precache-manifest.0fc74682a4ea4395b17614f136d4e547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d9b7cef8ea4611861e24aa4cb99401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a021380ca37fccfc6e7c",
+    "url": "/static/css/main.99debd4d.chunk.css"
+  },
+  {
+    "revision": "80510f084fccc8d10a0a",
+    "url": "/static/js/2.6b409dff.chunk.js"
+  },
+  {
+    "revision": "a021380ca37fccfc6e7c",
+    "url": "/static/js/main.746a3fa8.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "c2030d5be0b9612b93380594a7d873cd",
+    "url": "/static/media/applause.c2030d5b.mp3"
+  },
+  {
+    "revision": "baad2ca967e715ae6f4e9207471343f3",
+    "url": "/static/media/sad_trombone.baad2ca9.mp3"
+  }
+]);
