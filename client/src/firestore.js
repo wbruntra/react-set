3ccb621ceaa07234e7748c28e1884c417f6b74aa
William Bruntrager <w.bruntrager@gmail.com>
2019-09-26 05:06:19 -0400

    Update firebase config

--- client/src/firestore.js
+++ client/src/firestore.js
@@ -1,17 +1,10 @@
 // import * as firebase from 'firebase';
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import firebaseConfig from './firebaseConfig'
 
-const config = {
-  apiKey: 'AIzaSyAvSp91vcvkgt9RYafRIgY8noH4NSX0P0g',
-  authDomain: 'isthisprime.firebaseapp.com',
-  databaseURL: 'https://isthisprime.firebaseio.com',
-  projectId: 'isthisprime',
-  storageBucket: 'isthisprime.appspot.com',
-  messagingSenderId: '522572340456',
-}
-
-firebase.initializeApp(config)
+firebase.initializeApp(firebaseConfig)
+// firebase.analytics()
 
 const firestore = firebase.firestore()
 
