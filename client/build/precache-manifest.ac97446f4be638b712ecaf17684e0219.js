33eb4a43f1eb601849a47e060f4e3a162667f6c8
William Bruntrager <w.bruntrager@gmail.com>
2020-02-08 07:53:30 -0500

    Build

--- client/build/precache-manifest.ac97446f4be638b712ecaf17684e0219.js
+++ client/build/precache-manifest.ac97446f4be638b712ecaf17684e0219.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    revision: 'fe0362f3da9427a4ef52ea3464e40cf0',
-    url: '/index.html',
+    "revision": "fe0362f3da9427a4ef52ea3464e40cf0",
+    "url": "/index.html"
   },
   {
-    revision: '255f6738fa364985c9bd',
-    url: '/static/css/main.be4ce056.chunk.css',
+    "revision": "255f6738fa364985c9bd",
+    "url": "/static/css/main.be4ce056.chunk.css"
   },
   {
-    revision: 'edaa3ed5116171b37c02',
-    url: '/static/js/2.becb4126.chunk.js',
+    "revision": "edaa3ed5116171b37c02",
+    "url": "/static/js/2.becb4126.chunk.js"
   },
   {
-    revision: '255f6738fa364985c9bd',
-    url: '/static/js/main.0c7315de.chunk.js',
+    "revision": "255f6738fa364985c9bd",
+    "url": "/static/js/main.0c7315de.chunk.js"
   },
   {
-    revision: '875d570ce1b6fc43afc6',
-    url: '/static/js/runtime-main.e077b804.js',
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
   },
   {
-    revision: 'c2030d5be0b9612b93380594a7d873cd',
-    url: '/static/media/applause.c2030d5b.mp3',
+    "revision": "c2030d5be0b9612b93380594a7d873cd",
+    "url": "/static/media/applause.c2030d5b.mp3"
   },
   {
-    revision: 'baad2ca967e715ae6f4e9207471343f3',
-    url: '/static/media/sad_trombone.baad2ca9.mp3',
-  },
-])
+    "revision": "baad2ca967e715ae6f4e9207471343f3",
+    "url": "/static/media/sad_trombone.baad2ca9.mp3"
+  }
+]);
