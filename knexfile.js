ea679041dda1a4dad65a4b6143c3c4efa754e278
William Bruntrager <w.bruntrager@gmail.com>
2025-06-16 08:29:38 +0200

    Sqlite3

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,50 @@
 const secrets = require('./secrets')
 
 module.exports = {
+  // development: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     database: 'reactdb',
+  //     user: 'william',
+  //     password: 'battle',
+  //     port: 3366,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
+  // use better-sqlite3 instead of mysql2
   development: {
-    client: 'mysql2',
+    client: 'better-sqlite3',
     connection: {
-      database: 'reactdb',
-      user: 'william',
-      password: 'battle',
-      port: 3366,
+      filename: './react-set-dev.db',
     },
     migrations: {
       tableName: 'knex_migrations',
     },
   },
-
   production: {
-    client: 'mysql2',
+    client: 'better-sqlite3',
     connection: {
-      database: secrets.DB_NAME,
-      user: secrets.DB_USER,
-      password: secrets.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
+      filename: './react-set.db',
     },
     migrations: {
       tableName: 'knex_migrations',
     },
   },
+  // production: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     database: secrets.DB_NAME,
+  //     user: secrets.DB_USER,
+  //     password: secrets.DB_PASSWORD,
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
 }
