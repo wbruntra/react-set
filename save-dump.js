57f693f086af3555465f8455554f4c225a7cf511
William Bruntrager <w.bruntrager@gmail.com>
2025-07-19 11:24:39 +0200

    Add log

--- save-dump.js
+++ save-dump.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 
 const run = async () => {
   const games = await db('games').select()
+
+  console.log(`Found ${games.length} games in the database.`)
+
   fs.writeFileSync('./games_data.json', JSON.stringify(games, null, 2))
 }
 
