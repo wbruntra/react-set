83cc24f1d13516d1e07f5f906cdf51cf3f064ca0
William Bruntrager <w.bruntrager@gmail.com>
2026-06-10 08:52:40 +0200

    Update knexfile

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,6 +1,20 @@
 // knexfile.js
 const path = require('path')
 
+const commonConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(__dirname, 'react-db-production.sqlite3'),
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: path.join(__dirname, 'migrations'),
+  },
+  // Production-specific options
+  acquireConnectionTimeout: 60000,
+  asyncStackTraces: false,
+}
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -13,17 +27,6 @@ module.exports = {
     },
   },
 
-  production: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.join(__dirname, 'react-db-production.sqlite3'),
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: path.join(__dirname, 'migrations'),
-    },
-    // Production-specific options
-    acquireConnectionTimeout: 60000,
-    asyncStackTraces: false,
-  },
+  staging: commonConfig,
+  production: commonConfig,
 }
