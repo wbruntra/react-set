2571322d8847e86d3d31c3371a1ed28f8b7ccc98
William Bruntrager <w.bruntrager@gmail.com>
2026-06-11 17:44:46 +0200

    Update ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,23 @@
 const BASE = '/home/william/web'
 
 module.exports = {
-  name: 'react-set',
-  cwd: `${BASE}/react-set/backend/src`,
-  script: `index.ts`,
-  interpreter: '/usr/local/bin/bun',
-  exec_mode: 'fork',
-  instances: 1,
-  autorestart: true,
-  // add restart delay
-  restart_delay: 4000,
-  // add maximum restarts
-  max_restarts: 5,
-  watch: false,
-  max_memory_restart: '128M',
-  env: {
-    PORT: '5002',
-    NODE_ENV: 'production',
-  },
+  apps: [
+    {
+      name: 'react-set',
+      cwd: `${BASE}/react-set/backend/src`,
+      script: 'index.ts',
+      interpreter: '/usr/local/bin/bun',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      restart_delay: 4000,
+      max_restarts: 5,
+      watch: false,
+      max_memory_restart: '128M',
+      env: {
+        PORT: '5002',
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 }
